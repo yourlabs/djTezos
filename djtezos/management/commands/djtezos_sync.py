43c828b84e9c064b3c652555a5b361c5569e5d3a
jpic <jpic@yourlabs.org>
2021-06-06 19:50:33 +0200

    Prevent failing because of a blockchain endpoint problem

--- djtezos/management/commands/djtezos_sync.py
+++ djtezos/management/commands/djtezos_sync.py
@@ -1,3 +1,5 @@
+import logging
+
 from pytezos import pytezos
 
 from django.core.management.base import BaseCommand, CommandError
@@ -5,9 +7,15 @@ from django.core.management.base import BaseCommand, CommandError
 from djtezos.models import Blockchain, Contract, Call, Transaction
 
 
+logger = logging.getLogger('djtezos.djtezos_sync')
+
+
 class Command(BaseCommand):
     help = 'Synchronize external transactions'
 
     def handle(self, *args, **options):
         for blockchain in Blockchain.objects.filter(is_active=True):
-            blockchain.provider.watch_blockchain(blockchain)
+            try:
+                blockchain.provider.watch_blockchain(blockchain)
+            except Exception as exception:
+                logger.exception(exception)
